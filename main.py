325a62ff86e013aab52391b920f278bc7af00321
billgogi <fuda006@gmail.com>
2025-08-18 12:49:21 -0700

    E created circleshape.py and player.py to draw a player sprite on the screen, as well as updated main and constants to move said sprite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -9,6 +10,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    
     while True:
         
         for event in pygame.event.get():
@@ -16,8 +19,13 @@ def main():
                 return
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        player.update(dt)
         dt = clock.tick(60) / 1000
+
+
+        
 
 
 if __name__ == "__main__":
